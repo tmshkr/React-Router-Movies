7b4758bbe51cd4849de83f8489567d1ac1f7077f
Tim Shaker <tim@tmshkr.com>
2020-02-24 09:34:27 -0800

    MovieDetails onClick to Movie by id

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,36 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
+        .get("http://localhost:5000/api/movies")
         .then(response => {
+          console.log(response.data);
           setMovies(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails {...props} key={movie.id} movie={movie} />
       ))}
     </div>
   );
-}
+};
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+function MovieDetails(props) {
+  const { id, title, director, metascore, stars } = props.movie;
   return (
-    <div className="movie-card">
+    <div
+      className="movie-card"
+      onClick={() => props.history.push(`/movies/${id}`)}
+    >
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
